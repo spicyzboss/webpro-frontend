a4eab8468e117c47603e01960091cfd13ca56375
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-04-26 14:19:27 +0700

    fix: meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'patty friend and community' },
       { name: 'format-detection', content: 'telephone=no' },
-      { description: 'patty friend and community' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
