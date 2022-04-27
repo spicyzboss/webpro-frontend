cb4823c590c2de2deaa14abb4a8e0bb044761eab
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-04-28 04:56:57 +0700

    feat: add simple login

--- components/FormLogin.vue
+++ components/FormLogin.vue
@@ -1,10 +1,12 @@
 <template>
-  <form action="" class="p-12 bg-white border shadow-xl rounded-xl">
+  <div class="p-12 bg-white border shadow-xl rounded-xl">
     <div>
       <label for="email">Email</label>
       <input
         id="email"
         type="email"
+        name="email"
+        v-bind="email"
         placeholder="hello@emample.com"
         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#6667ba]"
       >
@@ -15,6 +17,8 @@
       <input
         id="password"
         type="password"
+        name="password"
+        v-bind="password"
         placeholder="password"
         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#6667ba]"
       >
@@ -25,12 +29,37 @@
       </nuxt-link>
     </div>
     <div class="flex flex-col items-center">
-      <button class="w-2/3 h-12 mt-4 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:outline-none focus:shadow-outline hover:bg-indigo-800 focus:bg-indigo-800">
+      <button class="w-2/3 h-12 mt-4 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:outline-none focus:shadow-outline hover:bg-indigo-800 focus:bg-indigo-800" @click="login">
         Login
       </button>
       <button class="w-2/3 h-12 mt-4 text-gray-500 transition-colors duration-150 bg-white border rounded-lg focus:outline-none focus:shadow-outline hover:bg-gray-100 focus:bg-gray-100">
         Register
       </button>
     </div>
-  </form>
+  </div>
 </template>
+
+<script>
+import { createHash } from 'crypto';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      const request = await this.$axios.$post('https://api.pattycommunity.com/login', {
+        email: this.email,
+        password: createHash('md5').update(this.password).digest('hex'),
+      });
+
+      if (request.status === 200) {
+        console.log(request);
+      }
+    },
+  },
+};
+</script>
--- components/NavLanding.vue
+++ components/NavLanding.vue
@@ -5,10 +5,10 @@
         <img class="h-12" src="patty-logo.png" alt="patty-logo">
       </nuxt-link>
       <div class="hidden gap-8 sm:flex">
-        <nuxt-link to="login" class="text-white hover:underline place-self-center focus:underline focus:outline-none">
+        <nuxt-link to="/login" class="text-white hover:underline place-self-center focus:underline focus:outline-none">
           Login
         </nuxt-link>
-        <nuxt-link to="register" class="px-4 py-2 text-black bg-white rounded-full place-self-center hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
+        <nuxt-link to="/register" class="px-4 py-2 text-black bg-white rounded-full place-self-center hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
           Sign Up
         </nuxt-link>
       </div>
