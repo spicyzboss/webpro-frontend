259c70a023c824d546ea8160768374e24de692d3
Chaowat Intarapanit <goten10_@hotmail.co.th>
2022-04-27 21:19:48 +0700

    login

---
+++ components/FormLogin.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="relative bg-white w-full h-max rounded border shadow-xl flex-col justify-center pb-12">
+    <div>
+      <p class="text-5xl font-bold text-center mt-[10%]">
+        Login
+      </p>
+    </div>
+    <div class="px-[10%] pt-5">
+      <form action="">
+        <div class="mt-[10%]">
+          <div>
+            <label class="block" for="username">Username<label>
+              <input
+                type="text"
+                placeholder="Username"
+                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              >
+              <span class="text-xs tracking-wide text-red-600 hidden">Email field is required </span>
+            </label></label>
+          </div>
+          <div class="mt-4">
+            <label class="block">Password<label>
+              <input
+                type="password"
+                placeholder="Password"
+                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+              >
+            </label></label>
+          </div>
+          <div class="flex flex-row-reverse mt-3">
+            <div><a href="#" class="text-sm text-blue-600 hover:underline">Forgot password?</a></div>
+          </div>
+          <div class="mt-4 w-full">
+            <div class="flex justify-center w-full"><button class="w-[60%] h-11 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Login</button></div>
+          </div>
+          <div class="mt-4 w-full">
+            <div class="flex justify-center w-full"><button class="w-[60%] h-11 px-6 border text-gray-500 transition-colors duration-150 bg-white rounded-lg focus:shadow-outline hover:bg-gray-300">register</button></div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
--- components/NavLanding.vue
+++ components/NavLanding.vue
@@ -2,9 +2,13 @@
   <div>
     <nav class="bg-[#6667ba] h-16">
       <div class="relative h-full w-[50%] mx-[25%] py-1 flex justify-between">
-        <div class="h-full w-auto"><a href="#"><img class="h-full w-auto p-1" src="../static/patty2.png" /></a></div>
-        <div class="h-full w-auto"></div>
-        <div class="h-full w-auto mt-4"><a href="#" class="bg-white hover:bg-gray-200 text-black text-center py-2 px-4 rounded-full">Login</a></div>
+        <div class="h-full w-auto">
+          <a href="../"><img class="h-full w-auto p-1" src="../static/patty2.png"></a>
+        </div>
+        <div class="h-full w-auto" />
+        <div class="h-full w-auto mt-4">
+          <a href="../HomeLogin" class="bg-white hover:bg-gray-200 text-black text-center py-3 px-5 rounded-full">Login</a>
+        </div>
       </div>
     </nav>
   </div>
