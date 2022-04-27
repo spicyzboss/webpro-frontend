4faa0f3ca251a84b42c7b99eddbb61ec16a9d87f
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-04-27 23:11:58 +0700

    chore: rename HomeLogin page to login

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
     <div class="h-max">
       <NavLanding />
     </div>
-    <div class="h-full mx-auto flex justify-center">
+    <div class="flex justify-center h-full mx-auto">
       <div class="relative w-[30vw] h-[50vh] mt-[5%] mr-5">
         <p class="text-4xl font-bold text-left text-[#f3799e]">
           <span class="text-6xl text-[#6667ba]">Sometime</span>  we don't want romantic relationship, we just want friend.<br><br><span class="text-6xl text-[#b66db0]">But</span><span class="text-[#ffc568]"> sometimes it can't be forced.</span>
