da62cd077fec982ac02fa1c29d280268ebdc9b5d
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-05-05 12:07:59 +0700

    chore: extract message component

---
+++ components/ReceiveMessage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-start mx-4 mt-4">
+    <div class="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded-lg shadow">
+      <span class="block">
+        <slot />
+      </span>
+    </div>
+  </div>
+</template>
---
+++ components/SentMessage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-end mx-4 mt-4">
+    <div class="relative max-w-xl px-4 py-2 text-white bg-blue-400 rounded-lg shadow">
+      <span class="block">
+        <slot />
+      </span>
+    </div>
+  </div>
+</template>
