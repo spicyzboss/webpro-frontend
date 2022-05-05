0299168b0d6150b3a02c0c347c47af6e9184b96a
Chaowat Intarapanit <goten10_@hotmail.co.th>
2022-05-05 23:38:29 +0700

    interest

--- pages/interest.vue
+++ pages/interest.vue
@@ -2,11 +2,11 @@
   <div class="h-screen">
     <NavLanding />
     <div class="h-full pt-16">
-      <div class="grid w-full h-full grid-cols-4">
-        <div class="w-full h-full col-span-2 overflow-y-auto bg-gray-200 border-r border-gray-300">
+      <div class="grid w-full h-full grid-cols-2">
+        <div class="w-full h-full overflow-y-auto bg-gray-200 border-r border-gray-300">
           <div class="relative flex items-center justify-center w-full h-12 bg-white border border-b">
             <div class="flex items-center justify-between w-full">
-              <p class="text-xl font-bold">
+              <p class="ml-2 text-xl font-bold">
                 Interest
               </p>
               <button class="flex items-center justify-center h-8 w-14 p-2 m-2 rounded-lg text-white bg-[#6667ba] hover:bg-[#494a86]">
@@ -37,7 +37,12 @@
             </div>
           </div>
         </div>
-        <div class="flex flex-col items-center w-full h-full col-span-2 overflow-y-auto bg-gray-200 border">
+        <div class="flex flex-col w-full h-full bg-gray-200 border">
+          <SelectInterest />
+          <SelectInterest />
+          <SelectInterest />
+          <SelectInterest />
+          <SelectInterest />
           <SelectInterest />
           <SelectInterest />
           <SelectInterest />
