3e9df29e705f7f298421ebe47167c782b2c2bf62
Chaowat Intarapanit <goten10_@hotmail.co.th>
2022-05-06 01:06:27 +0700

    LandingPage

--- pages/index.vue
+++ pages/index.vue
@@ -6,27 +6,18 @@
         <LandingPage />
       </div>
       <div v-else>
-        <div class="relative pb-4 bg-gray-100 ">
+        <div class="relative pb-4 bg-gray-100">
           <div class="h-2 xl:h-0" />
           <FeedFilter />
           <div class="flex flex-col h-full max-w-xl min-h-screen mx-auto">
             <FeedPostForm />
             <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
-            <FeedPost />
           </div>
         </div>
         <div class="pb-4 bg-gray-100">
-          <div class="flex flex-col items-center h-full max-w-xl min-h-screen mx-auto">
-            <FeedMatch />
-            <FeedMatch />
-            <FeedMatch />
-            <FeedMatch />
+          <div
+            class="flex flex-col items-center h-full max-w-xl min-h-screen mx-auto "
+          >
             <FeedMatch />
           </div>
         </div>
