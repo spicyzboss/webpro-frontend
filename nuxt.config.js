be4e92815b16b6e427eedd5a5fbed4eb2d25f4cc
Trda092 <73659917+Trda092@users.noreply.github.com>
2022-05-06 09:10:14 +0700

    nuxt congig

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,8 @@ export default {
   build: {},
 
   axios: {
-    // baseURL: 'https://api.pattycommunity.com',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://api.pattycommunity.com',
+    //baseURL: 'http://localhost:3000',
   },
 
   auth: {
